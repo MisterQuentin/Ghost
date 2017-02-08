3a63ed0ba23f67507a095b22306ae438005dbf7d
Alexis <eliard.alexis@gmail.com>
2017-02-08 15:41:54 +0100

    Add files via upload

---
+++ sources/JavaScript/all.js
@@ -0,0 +1,39 @@
+// $('body .content').append(
+	// $('<canvas />', {
+		// id: "Clear",
+		// css: {
+			// 'vertical-align': "middle",
+			// height: "100%",
+			// width: "0px"
+		// }
+	// })
+// );
+// $('body .content').append(
+	// $('<canvas />', {
+		// id: "Ghost-log",
+		// css: {
+			// 'vertical-align': "middle"
+		// }
+	// })
+// );
+// $("#Ghost-log").attr("height", "200px");
+// $("#Ghost-log").attr("width", "200px");
+// function drawConnexionButton() {
+	// var ctx = document.getElementById('Ghost-log').getContext('2d');
+	// ctx.clearRect(0,0,200,200);
+	// ctx.fillStyle = "rgb(245,245,245)";
+	// ctx.strokeStyle = "rgb(145,193,228)";
+	// ctx.fillRect(0,0,200,200);
+	// ctx.strokeRect(0.5,0.5,199,199);
+// }
+// drawConnexionButton();
+
+// /* 28 frame, on boucle sur les 8 derniére frame */
+
+// if($(window).width()<200) {
+	// $('body').html(
+		// $('<img />', {
+			// src: "SmallSize.png"
+		// })
+	// );
+// }
